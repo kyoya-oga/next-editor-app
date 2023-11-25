9420966dd5bdb1dc57a564da8ad5c9eefa2d722b
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2023-11-25 15:48:42 +0700

    added form validation.

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,28 +1,16 @@
-import { db } from '@/db';
-import { redirect } from 'next/navigation';
-import React from 'react';
+'use client';
+
+import * as actions from '@/actions';
+import { useFormState } from 'react-dom';
 
 type Props = {};
 
 export default function SnippetsCreatePage({}: Props) {
-  async function createSnippet(formData: FormData) {
-    'use server';
-
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    console.log(snippet);
-
-    redirect('/');
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: '',
+  });
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className='font-bold m-3'>Create a Snippet</h3>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
@@ -46,6 +34,12 @@ export default function SnippetsCreatePage({}: Props) {
             id='code'
           />
         </div>
+
+        {formState.message ? (
+          <div className='my-2 p-2 bg-red-100 border rounded border-red-400'>
+            {formState.message}
+          </div>
+        ) : null}
 
         <button type='submit' className='rounded p-2 bg-blue-200'>
           Create
