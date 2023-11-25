03a311f6a0561c9ed5b8656082bae1f811a948d4
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2023-11-25 09:45:30 +0700

    added editor library and server action

--- src/components/snippets-edit-form.tsx
+++ src/components/snippets-edit-form.tsx
@@ -1,11 +1,41 @@
 'use client';
 
 import { Snippet } from '@prisma/client';
+import { Editor } from '@monaco-editor/react';
+import { useState } from 'react';
+import { editSnippet } from '@/actions';
 
 type Props = {
   snippet: Snippet;
 };
 
 export default function SnippetEditForm({ snippet }: Props) {
-  return <div>client component snippet {snippet.title}</div>;
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = '') => {
+    setCode(value);
+  };
+
+  const editSnippetAction = editSnippet.bind(null, snippet.id, code);
+
+  return (
+    <div>
+      <Editor
+        height='40dvh'
+        theme='vs-dark'
+        language='javascript'
+        defaultValue={snippet.code}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
+        onChange={handleEditorChange}
+      />
+      <form action={editSnippetAction}>
+        <button type='submit' className='p-2 border rounded'>
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
